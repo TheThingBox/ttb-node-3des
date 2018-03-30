a8d292a6191f1402e0231adaf6c048ac660d197f
lucaspouchot <lucas.pouchot@digitalairways.com>
2018-03-30 14:03:19 +0200

    update

--- 61-decrypt.js
+++ 61-decrypt.js
@@ -1,26 +1,26 @@
 module.exports = function(RED) {
-    "use strict";
-    var crypto = require("crypto");
+  "use strict";
+  var crypto = require("crypto");
 
-    function Des3Decrypt(n) {
-        RED.nodes.createNode(this, n);
-        this.key = n.key;
-        var node = this;
-        this.on("input", function(msg) {
-            var key = node.key;
-            if(msg.key)
-                key = msg.key;
-            msg.payload = decrypt3DES(msg.payload, key);
-            node.send(msg);
-        });
-    }
-    RED.nodes.registerType("3DesDecrypt", Des3Decrypt);
+  function Des3Decrypt(n) {
+    RED.nodes.createNode(this, n);
+    this.key = n.key;
+    var node = this;
+    this.on("input", function(msg) {
+      var key = node.key;
+      if(msg.key) {
+        key = msg.key;
+      }
+      msg.payload = decrypt3DES(msg.payload, key);
+      node.send(msg);
+    });
+  }
+  RED.nodes.registerType("3DesDecrypt", Des3Decrypt);
 
-    function decrypt3DES(ciphertext, key) {
-        var iv = new Buffer("0000000000000000", "hex");
-        var hexkey = new Buffer(key, "hex");
-        var decipher = crypto.createDecipheriv("des3", hexkey, iv);
-        return decipher.update(ciphertext, "hex", "utf8") + decipher.final("utf8");
-    }
-
+  function decrypt3DES(ciphertext, key) {
+    var iv = new Buffer("0000000000000000", "hex");
+    var hexkey = new Buffer(key, "hex");
+    var decipher = crypto.createDecipheriv("des3", hexkey, iv);
+    return decipher.update(ciphertext, "hex", "utf8") + decipher.final("utf8");
+  }
 }
--- 61-encrypt.js
+++ 61-encrypt.js
@@ -1,28 +1,30 @@
 module.exports = function(RED) {
-    "use strict";
-    var crypto = require("crypto");
+  "use strict";
+  var crypto = require("crypto");
 
-    function Des3Encrypt(n) {
-        RED.nodes.createNode(this, n);
-        this.key = n.key;
-        var node = this;
-        this.on("input", function(msg) {
-            var key = node.key;
-            if(msg.key)
-                key = msg.key;
-            var payload = msg.payload;
-            if(typeof payload !== "string") payload = JSON.stringify(payload);
-            msg.payload = encrypt3DES(payload, key);
-            node.send(msg);
-        });
-    }
-    RED.nodes.registerType("3DesEncrypt", Des3Encrypt);
+  function Des3Encrypt(n) {
+    RED.nodes.createNode(this, n);
+    this.key = n.key;
+    var node = this;
+    this.on("input", function(msg) {
+      var key = node.key;
+      if(msg.key){
+        key = msg.key;
+      }
+      var payload = msg.payload;
+      if(typeof payload !== "string") {
+        payload = JSON.stringify(payload);
+      }
+      msg.payload = encrypt3DES(payload, key);
+      node.send(msg);
+    });
+  }
+  RED.nodes.registerType("3DesEncrypt", Des3Encrypt);
 
-    function encrypt3DES(plaintext, key) {
-        var iv = new Buffer("0000000000000000", "hex");
-        var hexkey = new Buffer(key, "hex");
-        var cipher = crypto.createCipheriv("des3", hexkey, iv);
-        return cipher.update(plaintext, "utf8", "hex") + cipher.final("hex");
-    }
-
+  function encrypt3DES(plaintext, key) {
+    var iv = new Buffer("0000000000000000", "hex");
+    var hexkey = new Buffer(key, "hex");
+    var cipher = crypto.createCipheriv("des3", hexkey, iv);
+    return cipher.update(plaintext, "utf8", "hex") + cipher.final("hex");
+  }
 }
